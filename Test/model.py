0d0127692371d30d2047c1e618111c7a500fbb31
rommuluslewis <rommuluslewis@gmail.com>
2025-10-07 16:50:30 +0800

    added readobs_point to python file generation

--- Test/model.py
+++ Test/model.py
@@ -11,10 +11,10 @@ glafic.set_secondary('flag_hodensity 2', verb=0)
 glafic.set_secondary('ran_seed -122000', verb=0)
 
 glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'anfw', 0.23, 8969773375008.418, 0.0, 0.0, 0.5659520122192441, 221.87744517263104, 80.79620116250311, 0)
+glafic.set_lens(1, 'anfw', 0.23, 363082888658.0666, 0.0, 0.0, 0.10995996200936457, 72.60042853112898, 58.54207260919006, 0)
 glafic.set_optlens(1, 1, 1, 1, 1, 1, 1, 1, 1)
 
-glafic.set_lens(2, 'pert', 0.23, 0.777, 0.0, 0.0, 0.0981741964114337, 48.97651507938833, 0, 0.5385645755193087)
+glafic.set_lens(2, 'pert', 0.23, 0.777, 0.0, 0.0, 0.01056905892454293, 211.0867016064577, 0, 0.25985619506562513)
 glafic.set_optlens(2, 1, 1, 1, 1, 1, 1, 1, 1)
 
 glafic.set_point(1, 0.777, 0.0, 0.0)
@@ -22,6 +22,7 @@ glafic.set_optpoint(1, 1, 1, 1)
 
 glafic.model_init(verb=0)
 
+glafic.readobs_point('Test/input.dat')
 glafic.optimize()
 glafic.findimg()
 glafic.write_crit(0.777)
