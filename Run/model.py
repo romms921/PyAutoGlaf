7b16638f0158c6682f1b25df84ff61ccd247624e
rommuluslewis <rommuluslewis@gmail.com>
2025-11-02 00:32:49 +0800

    fixed path differences

--- Run/model.py
+++ Run/model.py
@@ -1,6 +1,6 @@
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'OutputPOW+SHEAR', -3.5, -3.5, 3.5, 3.5, 0.01, 0.01, 1, verb=0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Output/POW+SHEAR', -3.5, -3.5, 3.5, 3.5, 0.01, 0.01, 1, verb=0)
 
 glafic.set_secondary('chi2_splane 1', verb=0)
 glafic.set_secondary('chi2_checknimg 0', verb=0)
@@ -10,10 +10,10 @@ glafic.set_secondary('hvary 0', verb=0)
 glafic.set_secondary('ran_seed -122000', verb=0)
 
 glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'pow', 0.23, 0.777, 0.0, 0.0, 0.29395959527756715, 0.9275321073690046, 0.6872088267299772, 1.8279643667359722)
+glafic.set_lens(1, 'pow', 0.23, 0.777, 0.0, 0.0, 0.4738250740430287, 257.8153660950159, 1.8329374602273016, 1.6241413566656207)
 glafic.setopt_lens(1, 1, 1, 1, 1, 1, 1, 1, 1)
 
-glafic.set_lens(2, 'pert', 0.23, 0.777, 0.0, 0.0, 0.08242740359712392, 248.28868123281873, 0, 0.2536914114724434)
+glafic.set_lens(2, 'pert', 0.23, 0.777, 0.0, 0.0, 0.04052025449932959, 199.39639786121018, 0, 0.18422473398359895)
 glafic.setopt_lens(2, 1, 1, 1, 1, 1, 1, 1, 1)
 
 glafic.set_point(1, 0.777, 0.0, 0.0)
@@ -21,7 +21,7 @@ glafic.setopt_point(1, 1, 1, 1)
 
 glafic.model_init(verb=0)
 
-glafic.readobs_point('Input/input.dat')
+glafic.readobs_point('Run/input.dat')
 glafic.optimize()
 glafic.findimg()
 glafic.writecrit(0.777)
