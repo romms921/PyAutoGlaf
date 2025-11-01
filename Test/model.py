0b068562996fa907e86a48c41c366ae2c9ada8c0
rommuluslewis <rommuluslewis@gmail.com>
2025-11-01 23:59:58 +0800

    checking code running status

--- Test/model.py
+++ Test/model.py
@@ -1,6 +1,6 @@
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'Test/POW+SHEAR', -3.5, -3.5, 3.5, 3.5, 0.01, 0.01, 1, verb=0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'OutputPOW+SHEAR', -3.5, -3.5, 3.5, 3.5, 0.01, 0.01, 1, verb=0)
 
 glafic.set_secondary('chi2_splane 1', verb=0)
 glafic.set_secondary('chi2_checknimg 0', verb=0)
@@ -10,10 +10,10 @@ glafic.set_secondary('hvary 0', verb=0)
 glafic.set_secondary('ran_seed -122000', verb=0)
 
 glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'pow', 0.23, 0.777, 0.0, 0.0, 0.1674784750498015, 69.72829099034578, 1.7644571318441427, 1.5552411125441372)
+glafic.set_lens(1, 'pow', 0.23, 0.777, 0.0, 0.0, 0.29395959527756715, 0.9275321073690046, 0.6872088267299772, 1.8279643667359722)
 glafic.setopt_lens(1, 1, 1, 1, 1, 1, 1, 1, 1)
 
-glafic.set_lens(2, 'pert', 0.23, 0.777, 0.0, 0.0, 0.09519319925679254, 54.79826382795356, 0, 0.3979538621248711)
+glafic.set_lens(2, 'pert', 0.23, 0.777, 0.0, 0.0, 0.08242740359712392, 248.28868123281873, 0, 0.2536914114724434)
 glafic.setopt_lens(2, 1, 1, 1, 1, 1, 1, 1, 1)
 
 glafic.set_point(1, 0.777, 0.0, 0.0)
@@ -21,7 +21,7 @@ glafic.setopt_point(1, 1, 1, 1)
 
 glafic.model_init(verb=0)
 
-glafic.readobs_point('Test/input.dat')
+glafic.readobs_point('Input/input.dat')
 glafic.optimize()
 glafic.findimg()
 glafic.writecrit(0.777)
